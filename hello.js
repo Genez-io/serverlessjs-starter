fb25dc07d6591d4d86ac543c8e8329da9cabdfe3
Costin-Robert Sin <sin.costinrobert@gmail.com>
2024-06-13 11:00:04 +0300

    Add Hello World serverless function example
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

---
+++ hello.js
@@ -0,0 +1,8 @@
+export const handler = async (event) => {
+    console.log("Hello World");
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" }),
+    };
+};
